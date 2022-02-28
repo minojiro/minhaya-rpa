68bd35ee0dd42bfd89a8541b001ff8dc7acc6bd2
minojiro <minojiro@users.noreply.github.com>
2022-02-28 18:40:25 +0900

    cosme

--- add_quiz.py
+++ add_quiz.py
@@ -7,8 +7,6 @@ SHORT_DELAY = 0.20
 
 gui.PAUSE = SHORT_DELAY
 
-PIXEL_RATIO = 2
-
 
 class AddQuiz():
     ADD_BTN_POS = None
@@ -16,10 +14,18 @@ class AddQuiz():
     SUGGEST_BTN_POS = None
     SAVE_BTN_POS = None
 
-    def get_img_pos(self, fname):
-        POS_X, POS_Y = gui.locateCenterOnScreen(
-            'ui_images/{}'.format(fname), confidence=0.8)
-        return [POS_X / PIXEL_RATIO, POS_Y / PIXEL_RATIO]
+    def add_quiz(self, csv_path):
+        with open(csv_path, encoding='utf8', newline='') as f:
+            csvreader = csv.reader(f)
+            # 先頭の行はスキップ
+            next(csvreader)
+            l = list(filter(lambda r: not r[0], [row for row in csvreader]))
+            lines_len = len(l)
+            for i, row in enumerate(l):
+                self.add_item(row[1], row[2])
+                sleep(SHORT_DELAY * 5)
+                print('{}/{} done'.format(i + 1, lines_len))
+        print("done")
 
     def add_item(self, q, a):
         if not self.ADD_BTN_POS:
@@ -32,9 +38,11 @@ class AddQuiz():
 
         # click add button
         gui.click(*self.ADD_BTN_POS)
+        sleep(SHORT_DELAY)
         gui.click(*self.ADD_BTN_POS)
 
         # input qa
+        sleep(SHORT_DELAY * 2)
         if not self.Q_FIELD_POS:
             self.Q_FIELD_POS = self.get_img_pos('question_field.png')
         gui.click(*self.Q_FIELD_POS)
@@ -54,7 +62,7 @@ class AddQuiz():
             self.SUGGEST_BTN_POS = self.get_img_pos('suggest_button.png')
         gui.click(*self.SUGGEST_BTN_POS)
         confirm_ok()
-        sleep(SHORT_DELAY * 10)
+        sleep(SHORT_DELAY * 5)
 
         # save
         if not self.SAVE_BTN_POS:
@@ -65,17 +73,8 @@ class AddQuiz():
 
         return True
 
-    def add_quiz(self, csv_path):
-
-        with open(csv_path, encoding='utf8', newline='') as f:
-            csvreader = csv.reader(f)
-            # 先頭の行はスキップ
-            next(csvreader)
-            l = list(filter(lambda r: not r[0], [row for row in csvreader]))
-            lines_len = len(l)
-            for i, row in enumerate(l):
-                self.add_item(row[1], row[2])
-                sleep(SHORT_DELAY * 5)
-                print('{}/{} done'.format(i + 1, lines_len))
-
-        print("done")
+    def get_img_pos(self, fname):
+        PIXEL_RATIO = 2
+        POS_X, POS_Y = gui.locateCenterOnScreen(
+            'ui_images/{}'.format(fname), confidence=0.8)
+        return [POS_X / PIXEL_RATIO, POS_Y / PIXEL_RATIO]
